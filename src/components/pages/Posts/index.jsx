bcc95ced2a3ba09ef1130425a12c799014b16da8
robotrainer <asbu26@gmail.com>
2024-06-09 13:58:03 +0300

    update

--- src/components/pages/Posts/index.jsx
+++ src/components/pages/Posts/index.jsx
@@ -1,7 +1,24 @@
+import { Link, useLoaderData } from "react-router-dom";
+
 export default function Posts() {
+  const posts = useLoaderData();
+
   return (
     <div>
       <h1>Posts page</h1>
+
+      <ol>
+        {posts.map((post) => {
+          return (
+            <li key={post.id}>
+              <Link to={`/posts/${post.id}`}>
+                <h4>{post.title}</h4>
+                <p>{post.body}</p>
+              </Link>
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 }
